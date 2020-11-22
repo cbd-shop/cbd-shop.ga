6906032a6bb69caf0951f946ff39c549944e05f4
cbd shop <luca.zoltan@gmail.com>
2020-11-22 19:05:33 +0100

    start

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Bejelentkezés &lsaquo; cbd &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/cbd-shop.ga/wp-includes/css/dashicons.min.css?ver=5.2.9' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/cbd-shop.ga/wp-includes/css/buttons.min.css?ver=5.2.9' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/cbd-shop.ga/wp-admin/css/forms.min.css?ver=5.2.9' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/cbd-shop.ga/wp-admin/css/l10n.min.css?ver=5.2.9' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/cbd-shop.ga/wp-admin/css/login.min.css?ver=5.2.9' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://cbd-shop.ga/wp-includes/css/dashicons.min.css?ver=5.2.9' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://cbd-shop.ga/wp-includes/css/buttons.min.css?ver=5.2.9' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://cbd-shop.ga/wp-admin/css/forms.min.css?ver=5.2.9' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://cbd-shop.ga/wp-admin/css/l10n.min.css?ver=5.2.9' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://cbd-shop.ga/wp-admin/css/login.min.css?ver=5.2.9' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://hu.wordpress.org/">A honlap lelke a WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/cbd-shop.ga/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="https://cbd-shop.ga/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Felhasználónév, vagy Email cím<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -34,13 +34,13 @@
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Emlékezzen rám</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Bejelentkezés" />
-				<input type="hidden" name="redirect_to" value="/wp-admin/" />
+				<input type="hidden" name="redirect_to" value="https://cbd-shop.ga/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="/cbd-shop.ga/wp-login.php?action=lostpassword">Elfelejtett jelszó?</a>
+					<a href="https://cbd-shop.ga/wp-login.php?action=lostpassword">Elfelejtett jelszó?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="/cbd-shop.ga/">
+			<p id="backtoblog"><a href="https://cbd-shop.ga/">
 		&larr; cbd	</a></p>
 			
 	</div>
