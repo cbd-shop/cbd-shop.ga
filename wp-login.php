21aede5e779590b59dcf014e0e80ee3c3be792e7
cbd shop <luca.zoltan@gmail.com>
2020-11-22 11:42:54 +0100

    my page

--- wp-login.php
+++ wp-login.php
@@ -7,7 +7,7 @@
 	<!--<![endif]-->
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Bejelentkezés &lsaquo; cbd shop &#8212; WordPress</title>
+	<title>Bejelentkezés &lsaquo; cbd &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
 <link rel='stylesheet' id='dashicons-css'  href='/https://cbd-shop.ga/wp-includes/css/dashicons.min.css?ver=5.2.9' type='text/css' media='all' />
 <link rel='stylesheet' id='buttons-css'  href='/https://cbd-shop.ga/wp-includes/css/buttons.min.css?ver=5.2.9' type='text/css' media='all' />
@@ -58,14 +58,11 @@
 			</script>
 
 			<p id="backtoblog"><a href="/https://cbd-shop.ga/">
-		&larr; cbd shop	</a></p>
+		&larr; cbd	</a></p>
 			
 	</div>
 
 	
-		<script>
-	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
-	</script>
 		<div class="clear"></div>
 	</body>
 	</html>
